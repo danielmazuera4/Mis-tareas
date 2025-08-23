8b2e5df05e8b4bb5b76bb7d44c7aaddaf69cc0f7
Aprendiz Informatica <apinf@SERVIASESORIA.LOCAL>
2025-08-23 12:38:41 -0500

    Agregados dj-database-url y gunicorn al requirements para deploy en Render

--- app/views.py
+++ app/views.py
@@ -24,7 +24,8 @@ def login_view(request):
             return redirect("task_list")
     else:
         form = AuthenticationForm()
-    return render(request, "login.html", {"form": form})
+    return render(request, "registration/login.html", {"form": form})
+
 
 def logout_view(request):
     logout(request)
